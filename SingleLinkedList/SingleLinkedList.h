490f695482983f94a06dcc099a1200f87a29e46a
Bhuwan Chopra <bhuwan.chopra@gmail.com>
2017-01-06 09:14:30 +0000

    Move the pointer handling to shared_ptr instead of raw pointers.

--- SingleLinkedList/SingleLinkedList.h
+++ SingleLinkedList/SingleLinkedList.h
@@ -11,34 +11,37 @@
 #include <cstddef>
 #include <cassert>
 #include <vector>
+#include <memory>
 
 using std::vector;
+using std::shared_ptr;
 
 template<class DataType>
 class SingleLinkedListNode {
-	SingleLinkedListNode* next_;
+	shared_ptr<SingleLinkedListNode<DataType>> next_;
 	DataType data_;
 public:
 	inline DataType getData() {
 		return data_;
 	}
-	inline SingleLinkedListNode* getNext() {
+	inline shared_ptr<SingleLinkedListNode<DataType>> getNext() {
 		return next_;
 	}
-	inline void setNext(SingleLinkedListNode<DataType>* ptr) {
+	inline void setNext(shared_ptr<SingleLinkedListNode<DataType>> ptr) {
 		next_ = ptr;
 	}
 	explicit SingleLinkedListNode(DataType value) :
-			SingleLinkedListNode(nullptr, value) {
-	}
-	SingleLinkedListNode(SingleLinkedListNode* head, DataType value) :
+		next_(nullptr), data_(value) {}
+
+	SingleLinkedListNode(shared_ptr<SingleLinkedListNode<DataType>> head, 
+		DataType value) :
 			next_(head), data_(value) {}
 };
 
 template<class DataType>
 class SingleLinkedList {
 public:
-	SingleLinkedListNode<DataType>* head_;
+	shared_ptr<SingleLinkedListNode<DataType>> head_;
 
 	SingleLinkedList(DataType value);
 
@@ -48,7 +51,7 @@ public:
 
 	bool remove(DataType value);
 
-	SingleLinkedListNode<DataType>* find(DataType value);
+	shared_ptr<SingleLinkedListNode<DataType>> find(DataType value);
 
 	vector<DataType> traversal();
 
@@ -56,9 +59,8 @@ public:
 };
 
 template<class DataType>
-SingleLinkedList<DataType>::SingleLinkedList(DataType value) {
-	head_ = new SingleLinkedListNode<DataType>(value);
-}
+SingleLinkedList<DataType>::SingleLinkedList(DataType value) :
+	head_(new SingleLinkedListNode<DataType>(value)) {}
 
 template<class DataType>
 SingleLinkedList<DataType>::SingleLinkedList() :
@@ -66,23 +68,22 @@ SingleLinkedList<DataType>::SingleLinkedList() :
 
 template<class DataType>
 void SingleLinkedList<DataType>::insert(DataType value) {
-	head_ = new SingleLinkedListNode<DataType>(head_, value);
+	head_.reset(new SingleLinkedListNode<DataType>(head_, value));
 }
 
 template<class DataType>
 bool SingleLinkedList<DataType>::remove(DataType value) {
-	SingleLinkedListNode<DataType>* node_to_delete = find(value);
+	shared_ptr<SingleLinkedListNode<DataType>> node_to_delete = find(value);
 	if (nullptr == node_to_delete) {
 		return false;
 	}
 
 	if (node_to_delete == head_) {
 		head_ = head_->getNext();
-		delete node_to_delete;
 		return true;
 	}
 
-	SingleLinkedListNode<DataType>* prev_ptr;
+	shared_ptr<SingleLinkedListNode<DataType>> prev_ptr;
 	for (prev_ptr = head_;
 		 nullptr != prev_ptr->getNext() && node_to_delete != prev_ptr->getNext();
 		 prev_ptr = prev_ptr->getNext())
@@ -92,16 +93,15 @@ bool SingleLinkedList<DataType>::remove(DataType value) {
 		return false;
 	} else {
 		prev_ptr->setNext(prev_ptr->getNext()->getNext());
-		delete node_to_delete;
 		return true;
 	}
 	assert(0);
 }
 
 template<class DataType>
-SingleLinkedListNode<DataType>* SingleLinkedList<DataType>::find(
+shared_ptr<SingleLinkedListNode<DataType>> SingleLinkedList<DataType>::find(
 		DataType value) {
-	SingleLinkedListNode<DataType>* p;
+	shared_ptr<SingleLinkedListNode<DataType>> p;
 	for (p = head_;
 		 nullptr != p && p->getData() != value;
 		 p = p->getNext())
@@ -126,7 +126,6 @@ template<class DataType>
 SingleLinkedList<DataType>::~SingleLinkedList() {
 	for(auto p = head_; nullptr != p;) {
 		auto nextp = p->getNext();
-		delete p;
 		p = nextp;
 	}
 }
